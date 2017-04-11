a9be5d2846776f5eab06eb2eb6a5423a9bd14ee2
Antti Koskinen <antti@koskinen.site>
2017-04-11 11:30:07 +0300

    All word findable

--- word.php
+++ word.php
@@ -1,15 +1,17 @@
 <?php
 $word = $_GET['word'];
-$word = mb_strtolower($word);
+//$word = mb_strtolower($word);
 $word = strip_tags($word);
 
 if ($word == "") {
-	die();
+    die();
 }
+
 $json       = file_get_contents("slangi.json");
 $dictionary = json_decode($json, true);
+$answers    = array_keys($dictionary, $word);
+$count      = count($answers);
 
-$answer = $dictionary[$word];
 
 echo "<link rel=\"stylesheet\" href=\"bootstrap/css/bootstrap.css\">";
 echo "<style type=\"text/css\">
@@ -17,17 +19,18 @@ echo "<style type=\"text/css\">
  </style>";
 echo "<center><h2><font color=\"#FFFFF\">";
 
-if ($answer == "") {
-	$reversedDictionary = array_flip($dictionary);
-	$answerReversed = $reversedDictionary[$word];
-	
-	if ($answerReversed != "") {
-		echo "$answerReversed</font></h2></center>";
-		die();
-	}
-	echo "Sanalle <i>$word</i> ei ole käännöstä.</font></center>";
-	die();
+if ($count == 0) {
+    $reversedDictionary = array_flip($dictionary);
+    $answers            = array_keys($reversedDictionary, $word);
+    $count              = count($answers);
+    
 }
-echo "$answer</h2></font></center>";
-
+if ($count == 0) {
+    echo "Ei hakutuloksia haulle \"$word\".";
+    die();
+}
+echo "$count sana(a) löytyi haulle $word:<br>";
+foreach ($answers as &$value) {
+    echo "$value<br>";
+}
 ?>
